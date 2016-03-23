5c23d8e6cb6024b0d7d2fee83aad7d0a882f2a5d
aarthiJ <aaart@umich.edu>
2016-03-23 10:51:59 -0400

    Update Test.m

--- Test.m
+++ Test.m
@@ -1,1 +1,16 @@
-print "bonjours le monde"
+X = [];
+%create low G key
+lG = uicontrol('Style','Pushbutton','Position',[0 100 80 40],'String','G','Callback', ...
+'Z=cos(2*pi*392*[1:2000]/7999);X=[X Z];sound(Z,7999);');
+
+%create A flat key
+Ab = uicontrol('Style','Pushbutton','Position',[175 500 50 100],'String','Ab / G#','Callback', ...
+'Z=cos(2*pi*418*[1:2000]/7999);X=[X Z];sound(Z,7999);');
+
+%create A key
+A = uicontrol('Style','Pushbutton','Position',[200 400 50 100],'String','A','Callback', ...
+'Z=cos(2*pi*440*[1:2000]/7999);X=[X Z];sound(Z,7999);');
+
+%create A sharp key
+Bb = uicontrol('Style','Pushbutton','Position',[225 500 50 100],'String','Bb / A#','Callback', ...
+'Z=cos(2*pi*466*[1:2000]/7999);X=[X Z];sound(Z,7999);');
