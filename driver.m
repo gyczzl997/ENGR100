e446e536dabba22365ddcf7506a1c755e66e00d7
kojimano <kojimano@umich.edu>
2016-03-30 12:24:57 -0400

    Update driver.m

--- driver.m
+++ driver.m
@@ -3,6 +3,8 @@ ORIGSOUND = audioread('proj3.wav');
 ORIGSOUND = reshape(ORIGSOUND, 32768, 13, 4);
 X = [];
 % Low G, A flat, A, A#, B, C, D flat, D, D#, E, F, F#, G 
-Var = [392, 418, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 798];
-Caharacters =['G', 'G#', 'A', 'A#', 'B', 'C', 'C#','D','D#','E','F','F#','G'];
-Synthesizer2(ORIGSOUND, X, Var, Caharacters);
+Var = [392 418 440 466 494 523 554 587 622 659 698 740 798];
+Characters =['G '; 'G#'; 'A '; 'A#'; 'B '; 'C '; 'C#'; 'D '; 'D#'; 'E '; 'F '; 'F#'; 'G '];
+
+Synthesizer2(ORIGSOUND, X, Var, Characters);
+
